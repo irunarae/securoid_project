b47c75bb4a8848c73eca32165df1a6d1727c2637
carrotyone <parent12@naver.com>
2013-05-28 19:38:32 +0900

    Class for Socket Communication
    소켓 통신을 위한 클래스들입니다.
    포트번호는 임시로 넣었고,
    테스트 환경이 여의치 않아 테스트는 하지 못했습니다.
    
    보내는 부분과 받는 부분에 대한 설명은 주석으로 달았고요.
    
    서버 쪽에서 테스트 부탁드립니다.
    
    아마 각각 두개의 파일로 나눠야 할 것 같고요,
    이미 PPP에서 경험했듯이 받는 쪽은 while(1) 안에서 계속해서 받아와야 할 것 같고,
    보내는 쪽은 아무때나 Socket 선언해서 보내면 될 것 같습니다.
    
    만약 서버쪽 일이 바빠 테스트가 불가할 경우
    금요일 내지 토요일을 이용해 함께 테스트해보고,
    조립하도록 하죠.
    
    이상입니다.
    김영근

--- socket.java
+++ socket.java
@@ -1,1 +1,70 @@
 //temp
+
+import java.io.*;
+import java.net.*;
+
+public class socket_server{
+	public static void main(String[] args) throws IOException
+	{
+		ServerSocket ss = new ServerSocket(1988);
+		//making socket for server with port number 1988 which is my birth year kk sorry to the young
+		
+		Socket sock = ss.accept();
+		//wait until completion of making connection with client
+		System.out.printIn("Server has connected "+sock.getInetAddress()+
+				"to the client with port number "+sock.getLocalPort());
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream());
+		//buffered reader which gets messages from socket
+		
+		PrintWriter pw = new PrintWriter(sock.getOutputStream(), true);
+		//print writer which sends messages via socket
+		
+		
+		String reader = br.readLine();
+		System.out.printIn("Rcvd : "+reader);
+		//get actual messages from buffered reader
+		//and print that
+		
+		pw.printIn(reader);
+		System.out.printIn("Sent : "+reader);
+		//send the message from the client to test
+		
+		pw.close();
+		br.close();
+		sock.close();
+		ss.close();
+		//all should be closed after working
+	}
+}
+
+public class socket_client{
+	public static void main(String[] args) throws IOException{
+		String s = "Input to the server";
+		//this is just test input
+		
+		Socket sock = new Socket("163.152.161.155", 1988);
+		System.out.printIn("Attempt to make connection to address, 163.152.161.155 with port number 1988");
+		//making socket for client with temperature ip addr. and port number 1988 which i defined in socket_server class
+		
+		System.out.printIn("Will be sent : "+s);
+		PrintWriter pw = new PrintWriter(sock.getOutputStream(), true);
+		//print writer which sends messages via socket
+		pw.printIn(s);
+		//send the message to server for test
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		//buffered reader which gets messages from socket
+		
+		String reader = br.readLine();
+		System.out.printIn("Rcvd : "+reader);
+		//get actual messages from buffered reader
+		//and print that
+		
+		br.close();
+		pw.close();
+		sock.close();
+		//all should be closed after working
+	}
+}
